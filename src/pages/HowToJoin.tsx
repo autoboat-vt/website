72620bca4ed163d383d4e662520e99e1e3915559
Christopher Nassif <chrisjnassif@gmail.com>
2026-07-20 01:50:40 -0400

    style Join Now! link with white text and underline in dark mode

--- src/pages/HowToJoin.tsx
+++ src/pages/HowToJoin.tsx
@@ -9,6 +9,7 @@ export default function HowToJoin() {
                         href="https://docs.google.com/forms/d/e/1FAIpQLScViTYzAYHl671SGp_dmYO9MvrleM3VWAem8Ep7FROBCVOB9Q/viewform?usp=sharing&ouid=116978117130571796595"
                         target="_blank"
                         rel="noopener noreferrer"
+                        className="text-fontcolor dark:text-white underline decoration-current"
                     >
                         Join Now!<span className="sr-only"> (opens in a new tab)</span>
                     </a>
